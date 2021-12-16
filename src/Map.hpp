2030b58832475c8220a4ec90ada8ddb4df439853
mantonc01 <91096244+mantonc01@users.noreply.github.com>
2021-12-16 19:22:40 +0100

    fin clase no funciona

--- src/Map.hpp
+++ src/Map.hpp
@@ -8,8 +8,8 @@
 #define ALTO_MAPA 50
 
 struct Tile {
-  bool explored;//esta explorado?
-  Tile():explored(false){};
+   bool explored; //Está explorado?
+   Tile() : explored(false) {}
 };
 
 
@@ -26,12 +26,17 @@ public :
   void dig(int x_inicial, int y_inicial, int x_final, int y_final);
   void createRoom(bool first, int x1, int y1, int x2, int y2);
 
-protected :
+  bool isInFov(int x, int y) const;
+  bool isExplored(int x, int y) const;
+  void computeFov();
+  bool canWalk(int x, int y)const;
+  void addMonster(int x, int y);
 
+protected :
   Tile *tiles;
   TCODMap *map;
 
   void setWall(int x, int y);
-  void setWalkable(int x, int y); //Para hacer dig!
+  void setWalkable(int x, int y ); //Para hacer dig!
 };
 
